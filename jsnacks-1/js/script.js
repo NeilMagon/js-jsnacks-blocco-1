0474feb05133034e94d53d0ca8a579632de64fd7
NeilMagon <neil.magon97@gmail.com>
2024-03-11 16:27:15 +0100

    feat: creation array and for

---
+++ jsnacks-1/js/script.js
@@ -0,0 +1,9 @@
+// - Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati
+//  (inserire il numero nell'array solo se non è già presente).
+
+const numberPc = [];
+
+for (let i = 0; i < 5; i++) {
+    const userNumber = parseInt(prompt(`Dimmi un numero`));
+    
+}

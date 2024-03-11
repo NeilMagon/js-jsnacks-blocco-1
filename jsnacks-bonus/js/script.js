7d54f852dc3fea893d7d1e1a9f8ed36ac1401573
NeilMagon <neil.magon97@gmail.com>
2024-03-11 17:03:31 +0100

    feat: add let bigNumber

---
+++ jsnacks-bonus/js/script.js
@@ -0,0 +1,10 @@
+// BONUS:
+// - Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+// Creo un array
+const numberPc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numberPc);
+
+// Creo variabile per stampare in console il numero più grande
+let bigNumber = Math.max(...numberPc);
+console.log(bigNumber);

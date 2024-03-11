f1d1d1d7bea13c84fdddadb51c7e6a38dba4efe9
NeilMagon <neil.magon97@gmail.com>
2024-03-11 16:52:52 +0100

    feat: add for and media

--- jsnacks-4/js/script.js
+++ jsnacks-4/js/script.js
@@ -2,3 +2,16 @@
 
 // Creo l'array
 const numberPc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numberPc);
+
+let total = 0
+
+// Creo un ciclo for per calcolare la somma dell'array
+for (let i = 0; i < numberPc.length; i++) {
+    total += numberPc[i];
+}
+console.log(total);
+
+// Creo una variabile per calcolare la media
+let media = total % numberPc.length;
+console.log(media)

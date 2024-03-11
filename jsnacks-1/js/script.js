6d4a9184d6e6064f747c7e2d80d8a0c707268031
NeilMagon <neil.magon97@gmail.com>
2024-03-11 16:31:35 +0100

    feat: add if

--- jsnacks-1/js/script.js
+++ jsnacks-1/js/script.js
@@ -7,5 +7,11 @@ const numberPc = [];
 // Creo un ciclo for per chiedere i numeri all'utente
 for (let i = 0; i < 5; i++) {
     const userNumber = parseInt(prompt(`Dimmi un numero`));
-    
+    console.log(userNumber);
+    // Se i numeri son già inclusi nell'array non li inserisco
+    if (numberPc.includes(userNumber) === false) {
+        // Se i numeri non sono inclusi nell'array li inserisco
+        numberPc.push(userNumber);
+    }
 }
+console.log(numberPc)
